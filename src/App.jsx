b6cec9e277c482ae581b4873a5ee162b6bbe5c3b
Carlos Esteban Murillo <42922050+carban@users.noreply.github.com>
2025-03-18 16:09:21 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function SearchEngine() {
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
   const url = "https://b4c9-52-91-143-124.ngrok-free.app";
 
   function getYouTubeVideoId(url) {
@@ -13,6 +14,7 @@ export default function SearchEngine() {
 
   const handleSearch = async (e) => {
     e.preventDefault();
+    setLoading(true);
     // const data = {"answer":[{"href":"/clases/11887-reactjs/74504-que-es-react-y-cuales-son-sus-casos-de-uso/","image":"https://static.platzi.com/media/achievements/reactjs-patrones-render_badge-3c77e155-b04a-4c95-ac61-e2b6d2d5b92e-00753ce_R9qSoA6.webp","title":"Curso de React.js","teacher":"Por Estefany Aguilar","videos":[{"title":"¿Qué es ReactJS? ⚛️ ¿Cómo funciona? Explicación animada","url":"https://www.youtube.com/watch?v=bvxm389cYVI&pp=ygUIUmVhY3QuanM%3D"},{"title":"¿Qué es React.js y cómo funciona? - La mejor explicación en español","url":"https://www.youtube.com/watch?v=lWQ69WX7-hA&pp=ygUIUmVhY3QuanM%3D"},{"title":"Aprende React en 15 Minutos 📘","url":"https://www.youtube.com/watch?v=wGxDfSWC4Ww&pp=ygUIUmVhY3QuanM%3D"}],"title_emb":"curso de react.js"},{"href":"/clases/3219-react-redux-profesional/51177-ya-tomaste-el-curso-basico-de-redux/","image":"https://static.platzi.com/media/achievements/profesional-reactjs-redux-badge-d70e22d4-8d39-48be-a2a4-82945cd02e16.png","title":"Curso Profesional de React.js y Redux","teacher":"Por Mariangélica Useche","videos":[{"title":"Curso Profesional de React (firebase, hooks, redux, MaterialUI)","url":"https://www.youtube.com/watch?v=5DAEdXKp7QA&pp=ygUlQ3Vyc28gUHJvZmVzaW9uYWwgZGUgUmVhY3QuanMgeSBSZWR1eA%3D%3D"},{"title":"1. REACT con REDUX - CURSO PROFESIONAL","url":"https://www.youtube.com/watch?v=w8rRCNjj5W4&pp=ygUlQ3Vyc28gUHJvZmVzaW9uYWwgZGUgUmVhY3QuanMgeSBSZWR1eA%3D%3D"},{"title":"Curso Completo de React - La guía definitiva: Hooks, router, redux, next y muchos proyectos!","url":"https://www.youtube.com/watch?v=CvxPD5Acwgc&pp=ygUlQ3Vyc28gUHJvZmVzaW9uYWwgZGUgUmVhY3QuanMgeSBSZWR1eA%3D%3D"}],"title_emb":"curso profesional de react.js y redux"},{"href":"/clases/5481-react-typescript/57804-el-presente-del-frontend-es-typescript/","image":"https://static.platzi.com/media/achievements/piezas-react-typescript_badge-0b482f25-d778-492a-9cd8-d3b351e45667.png","title":"Curso de React.js con TypeScript","teacher":"Por Jonathan Alvarez","videos":[{"title":"Tutorial práctico: React y TypeScript paso a paso, crea tu primera aplicación","url":"https://www.youtube.com/watch?v=4lAYfsq-2TE&pp=ygUXUmVhY3QuanMgY29uIFR5cGVTY3JpcHQ%3D"},{"title":"How to use TypeScript with React... But should you?","url":"https://www.youtube.com/watch?v=ydkQlJhodio&pp=ygUXUmVhY3QuanMgY29uIFR5cGVTY3JpcHQ%3D"},{"title":"¿Utilizar REACT con Javascript o con Typescript? Invitado @midudev","url":"https://www.youtube.com/watch?v=rbPFyY-eGjQ&pp=ygUXUmVhY3QuanMgY29uIFR5cGVTY3JpcHQ%3D"}],"title_emb":"curso de react.js con typescript"},{"href":"/clases/2167-reactividad-vuejs/35415-como-convertirte-en-frontend-developer-con-vuejs/","image":"https://static.platzi.com/media/achievements/badge-vue-3-4b4c3cb0-ea25-4eac-957f-28eb09339383.png","title":"Curso de Reactividad con Vue.js 3","teacher":"Por Samuel Burbano","videos":[{"title":"Cómo usar la Reactividad en Vue 3","url":"https://www.youtube.com/watch?v=RVtsIAVPORA&pp=ygUYUmVhY3RpdmlkYWQgY29uIFZ1ZS5qcyAz"},{"title":"☘ Curso en Vue 3: Conociendo el sistema reactivo: REACTIVIDAD en Potencia #7","url":"https://www.youtube.com/watch?v=8rU5yervles&pp=ygUYUmVhY3RpdmlkYWQgY29uIFZ1ZS5qcyAz"},{"title":"La reactividad y componentes en un Proyecto Vue 3 | Curso de Vue.js 3 - 03","url":"https://www.youtube.com/watch?v=oeCfkrNyTk0&pp=ygUYUmVhY3RpdmlkYWQgY29uIFZ1ZS5qcyAz"}],"title_emb":"curso de reactividad con vue.js 3"}]}
     // setResults(data.answer); // Simulate search results
     const response = await fetch(url, {
@@ -23,6 +25,7 @@ export default function SearchEngine() {
       body: JSON.stringify({ question: searchTerm }),
     });
     const data = await response.json();
+    setLoading(false);
     setResults(data.answer)
   };
 
@@ -44,7 +47,12 @@ export default function SearchEngine() {
           </button>
         </form>
       </div>
-      {results.length > 0 && (
+      {loading && (
+        <div className="loader-container">
+          <div className="loader"></div>
+        </div>
+      )}
+      {(results.length > 0 && !loading) && (
         <div className="mt-10 w-full max-w-7xl bg-white shadow-lg rounded-lg overflow-hidden">
           <table className="w-full text-left border-collapse">
             <thead>
